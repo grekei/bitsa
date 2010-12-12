8fd48a4ea31910b744f278e7bf1e382532c0822c
Greg McKay <gkmckay@gmail.com>
2010-12-12 21:05:31 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitsa::Application.config.secret_token = '2e982f54dac4ffd9414a3fdcb4bc2e539fab4b20bf6f46b5d68b763937fdebf42c966accff8207fbb3e7d6af6a8e987102323fe7031d59d9e317e7c32ee6a994'
